7a0606fcf2ec2c96978a4acde1a8516106ab944f
Geektree0101 <h2s1880@gmail.com>
2021-04-25 17:50:01 +0900

    add profile scene components

---
+++ lib/screen/profile/components/profile_content_view.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ProfileContentView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext cotnext) {
+
+    return Container();
+  }
+}
---
+++ lib/screen/profile/profile_scene.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class ProfileScene extends StatefulWidget {
+  
+  // key
+  ProfileScene({Key key}) : super(key: key);
+
+  // createState
+  @override
+  ProfileSceneState createState() => ProfileSceneState();
+}
+
+class ProfileSceneState extends State<ProfileScene> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+}
